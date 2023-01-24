ff9d2e0020810a9c7ccb6a48a1b9849c7a1423eb
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-01-24 22:41:00 +0545

    Add files via upload

--- random-walker.c
+++ random-walker.c
@@ -1,75 +1,113 @@
-//Random Walker By Kushal Pathak
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<time.h>
-#define h 25
-#define w 25
+// Random Walker By Kushal Pathak
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <windows.h>
+#define h 49
+#define w 105
 #define path 'X'
-char left='<', right='>', up='^', down='v';
+char left = '<', right = '>', up = '^', down = 'v';
 char walker = '>';
-int x=12,y=12,dir=1, matrix[h][w];
+int x = w / 2, y = h / 2, dir = 1, matrix[h][w];
 
-void init();//To fill matrix with empty spaces
-void display();//To display contents of matrix
-void walk();//To walk in a random direction by one unit
-void delay(float);//To create delay 
-void random();//To generate a random direction
+void maximizeWindow();
+void init();	   // To fill matrix with empty spaces
+void display();	   // To display contents of matrix
+void walk();	   // To walk in a random direction by one unit
+void delay(float); // To create delay
+void random();	   // To generate a random direction
 
-void main(){
+void main()
+{
+	maximizeWindow();
 	init();
-	int i,n;
+	int i, n;
 	float speed;
 	printf("Random Walker By Kushal Pathak\nEnter steps (eg 100): ");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	printf("Enter speed (eg 0.3): ");
-	scanf("%f",&speed);
-	for(i=0;i<=n;i++){
+	scanf("%f", &speed);
+	for (i = 0; i <= n; i++)
+	{
 		display();
-		printf("\n--------------------(Step %d)--------------------",i);
+		printf("--------------------(Step %d)--------------------", i);
 		delay(speed);
 		walk();
 	}
 	getch();
 }
-void init(){
+void init()
+{
 	srand(time(0));
-	int i,j;
-	for(i=0;i<h;i++){
-		for(j=0;j<w;j++)
-		matrix[i][j]=' ';
+	int i, j;
+	for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w; j++)
+			matrix[i][j] = ' ';
 	}
-	matrix[12][12]=path;
+	matrix[h / 2][w / 2] = path;
 }
-void walk(){
+void walk()
+{
 	random();
-	switch(dir){
-		case 0: y=(y+h-1)%h; walker=up; break;
-		case 1: x=(x+1)%w; walker=right; break;
-		case 2: y=(y+1)%h; walker=down; break;
-		case 3: x=(x+w-1)%w; walker=left; break;
+	switch (dir)
+	{
+	case 0:
+		y = (y + h - 1) % h;
+		walker = up;
+		break;
+	case 1:
+		x = (x + 1) % w;
+		walker = right;
+		break;
+	case 2:
+		y = (y + 1) % h;
+		walker = down;
+		break;
+	case 3:
+		x = (x + w - 1) % w;
+		walker = left;
+		break;
 	}
-	matrix[y][x]=path;
+	matrix[y][x] = path;
 }
-void display(){
+void display()
+{
 	system("cls");
-	int i,j;
-	for(i=0;i<h;i++){
+	int i, j;
+	for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w; j++)
+		{
+			if (j == x && i == y)
+				printf("%c ", walker);
+			else
+				printf("%c ", matrix[i][j]);
+		}
+
 		printf("\n");
-		for(j=0;j<w;j++)
-		if(j==x&&i==y)printf("%c ",walker);
-		else printf("%c ",matrix[i][j]);
 	}
 }
-void delay(float n){
+void delay(float n)
+{
 	int i, j, k;
-	for(i=0;i<10000;i++){
-		for(j=0;j<500;j++){
-			for(k=0;k<n*100;k++);
+	for (i = 0; i < 10000; i++)
+	{
+		for (j = 0; j < 500; j++)
+		{
+			for (k = 0; k < n * 100; k++)
+				;
 		}
 	}
 }
-void random(){
-	dir=rand()%4;
+void random()
+{
+	dir = rand() % 4;
+}
+void maximizeWindow()
+{
+	HWND hwnd = GetConsoleWindow();
+	ShowWindow(hwnd, SW_SHOWMAXIMIZED);
 }
